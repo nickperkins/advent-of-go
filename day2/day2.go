f1700ab1e32c19569df28fe3a5c422604f66cfae
Nick Perkins <n.perkins@arkoselabs.com>
2020-12-02 15:50:44 +1000

    Day 02 Clean up output

--- day2/day2.go
+++ day2/day2.go
@@ -1,7 +1,6 @@
 package day2
 
 import (
-	"fmt"
 	"regexp"
 	"strconv"
 	"strings"
@@ -28,10 +27,8 @@ func DayTwoPartOne(input string) int {
 		low, _ := strconv.Atoi(matchGroups[1])
 		high, _ := strconv.Atoi(matchGroups[2])
 		charCount := strings.Count(matchGroups[4], matchGroups[3])
-		fmt.Printf("%v %v %v\n", low, high, charCount)
 		if charCount >= low && charCount <= high {
 			valid++
-			fmt.Println("Valid")
 		}
 	}
 
@@ -48,11 +45,8 @@ func DayTwoPartTwo(input string) int {
 		pos2, _ := strconv.Atoi(matchGroups[2])
 		match1 := string(matchGroups[4][pos1-1])
 		match2 := string(matchGroups[4][pos2-1])
-		//charCount := strings.Count(matchGroups[4], matchGroups[3])
-		//fmt.Printf("%v %v %v\n", low, high, charCount)
 		if match1 != match2 && (match1 == matchGroups[3] || match2 == matchGroups[3]) {
 			valid++
-			fmt.Println("Valid")
 		}
 	}
 
